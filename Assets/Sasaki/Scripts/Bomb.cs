2394e85ea1ef525a67b3fdb77b017ced52ef3138
Sasaki <sdt22cge0011@edu.sca.ac.jp>
2023-07-20 16:27:32 +0900

    ヘリコプターの挙動(HP以外)

---
+++ Assets/Sasaki/Scripts/Bomb.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+            //ここでダメージ計算
+        }
+        if (other.gameObject.tag == "Stage")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Sasaki/Scripts/Helicopter.cs
+++ Assets/Sasaki/Scripts/Helicopter.cs
@@ -4,72 +4,171 @@ using UnityEngine;
 
 public class Helicopter : MonoBehaviour
 {
-    private float sin;
+    //移動関係
+    private float cos;
     private float HorizontalWidth = 14.0f;//横移動の幅
-    private float VerticalWidth = 1.5f;//垂直移動の幅
-
-    private float Height = 3.5f;//高さ調整
-    private float Width = 6.5f;
+    private float HorizontalHeight = 3.5f;//横移動の時の高さ
+    private float VerticalWidth = 6.0f;//垂直移動のX座標
+    private float VerticalHeight = 2.0f;//垂直移動の高さ(幅)
+    private float height = 1.0f;//垂直移動の高さ調整
     private float speed = 2.0f;//移動速度
+    private int MoveCount = 0;
+    private int ChangeCount = 4;//移動の向き変更
+    private int MaxCount = 10;
+    private bool lotateCheck = false;//回転するかの確認
+    private bool VerticalMove = false;
+    private bool HorizontalMove = false;
+    private bool CountCheck = false;
 
-    private int Move = 0;
-    private int Rotate = 0;
-    private float MoveTime = 6.0f;
+    //攻撃関係
+    [SerializeField]
+    private GameObject Bomb;
+    [SerializeField]
+    private GameObject HelicopterBullet;
+    private GameObject player;
+    private float BombTimer;//爆弾投下の時間
+    private float MinBombTime = 0.1f;
+    private float MaxBombTime = 0.3f;
+    private float XshotPosition = 2.0f;
+    private float YshotPosition = 2.0f;
+    private float BulletSpeed = 6.0f;
+    private int AttackCount = 6;
+    private bool AttackCheck = true;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.FindGameObjectWithTag("Player");
+        VerticalMove = true;
+        BombTimer = Random.Range(MinBombTime,MaxBombTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //移動の向き変更
-        MoveTime -= Time.deltaTime;
-        if (MoveTime <= 0.0f)
+        cos = Mathf.Cos(Time.time * speed);
+
+
+        //Countでヘリコプターの移動を変えている
+        if (MoveCount == ChangeCount)
         {
-            MoveTime = 6.0f;
-            if (Move == 0)
+            VerticalMove = false;
+            HorizontalMove = true;
+            VerticalHeight = 8.0f;
+            height = 7.0f;
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+        }
+        if (MoveCount == ChangeCount+1)
+        {
+            VerticalHeight = 2.0f;
+            height = 1.0f;
+        }
+        if (MoveCount == AttackCount)
+        {
+            if (AttackCheck)
             {
-                Height = 1.0f;
-                Move = 1;
+                AttackCheck = false;
+                BulletsAttack();
             }
-            else if(Move == 1)
+        }
+        if (MoveCount == MaxCount-1)
+        {
+            VerticalHeight = 8.0f;
+            height = 7.0f;
+        }
+        if (MoveCount == MaxCount)
+        {
+            MoveCount = 0;
+            VerticalMove = true;
+            HorizontalMove = false;
+            AttackCheck = true;
+            transform.rotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
+        }
+
+
+        //水平移動
+        if (VerticalMove)
+        {
+            transform.position = new Vector3(cos * HorizontalWidth, HorizontalHeight, 0);
+
+            if (transform.position.x >= -4.0f && transform.position.x <= 4.0f)
             {
-                Height = 3.5f;
-                Move = 0;
+                BombTimer -= Time.deltaTime;
+            }
+            if (BombTimer <= 0.0f)
+            {
+                BombTimer = Random.Range(MinBombTime, MaxBombTime);
+                BombAttack();
             }
         }
 
-        //移動
-        sin = Mathf.Sin(Time.time * speed);
-        if (Move == 0)
+        //垂直移動
+        if (HorizontalMove)
         {
-            transform.position = new Vector3(sin * HorizontalWidth, Height, 0);
+            transform.position = new Vector3(VerticalWidth, cos * VerticalHeight + height,0);
         }
-        if (Move == 1)
+
+
+        //cosの値で出現のタイミングを調整
+        if (cos <= -0.9f || cos >= 0.9f)
         {
-            transform.position = new Vector3(Width, sin * VerticalWidth + Height, 0);
+            if (CountCheck)
+            {
+                CountCheck = false;
+                ++MoveCount;
+            }
         }
+        else
+        {
+            CountCheck = true;
+        }
+
 
         //回転
-        if (transform.position.x <= -13.0f)
+        if (transform.position.x <= -13.0f || transform.position.x >= 13.0f)
         {
-            if (Rotate == 0)
+            if (lotateCheck)
             {
-                Rotate = 1;
-                transform.Rotate(0, 180, 0);
+                TurnAround();
             }
         }
-        if (Rotate == 1)
+        else
         {
-            if (transform.position.x >= 13.0f)
-            {
-                Rotate = 0;
-                transform.Rotate(0, 180.0f, 0);
-            }
+            lotateCheck = true;
         }
+    }
 
+    //回転の関数
+    void TurnAround()
+    {
+        lotateCheck = false;
+        transform.Rotate(0, 180, 0);
+    }
 
+    void BombAttack()
+    {
+        Instantiate(Bomb,this.transform.position,Quaternion.identity);
+    }
+
+    void BulletsAttack()
+    {
+        Vector3 targetPosition = player.transform.position;
+        StartCoroutine(BulletsShot(targetPosition));
+    }
+
+    IEnumerator BulletsShot(Vector3 targetPosition)
+    {
+        for (int i = 0; i < 5; ++i)
+        {
+            Vector3 shotPosition = new Vector3(XshotPosition,YshotPosition,0);
+            GameObject Bullet = Instantiate(HelicopterBullet,shotPosition,Quaternion.identity);
+            Rigidbody2D BulletRigidbody = Bullet.GetComponent<Rigidbody2D>();
+            if (BulletRigidbody != null)
+            {
+                Vector3 dir = (targetPosition - shotPosition).normalized;
+                BulletRigidbody.velocity = dir * BulletSpeed;
+            }
+            yield return new WaitForSeconds(0.2f);
+        }
     }
 }
